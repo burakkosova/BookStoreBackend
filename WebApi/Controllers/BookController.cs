d957c40a0b617bb96ff788f5a1b143120eb7379e
Burak Kosova <burak.kosova25@gmail.com>
2022-03-20 23:57:37 +0300

    crud operations added

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.Controllers
 {
@@ -10,34 +11,17 @@ namespace WebApi.Controllers
   [Route("[controller]s")]
   public class BookController : ControllerBase
   {
-    private static List<Book> BookList = new List<Book>() {
-      new Book{
-        Id = 1,
-        Title="Lean Startup",
-        GenreId=1, // Personal Growth,
-        PageCount = 200,
-        PublishDate = new DateTime(2001,06,12)
-      },
-      new Book{
-        Id = 2,
-        Title="Herland",
-        GenreId=2, // Science Finction
-        PageCount = 250,
-        PublishDate = new DateTime(2010,05,23)
-      },
-      new Book{
-        Id = 3,
-        Title="Dune",
-        GenreId=2, // Personal Growth,
-        PageCount = 200,
-        PublishDate = new DateTime(2001,12,21)
-      }
-    };
+    private readonly BookStoreDbContext _context;
+
+    public BookController(BookStoreDbContext context)
+    {
+      _context = context;
+    }
 
     [HttpGet]
     public List<Book> GetBooks()
     {
-      var bookList = BookList.OrderBy(x => x.Id).ToList<Book>();
+      var bookList = _context.Books.OrderBy(x => x.Id).ToList<Book>();
       return bookList;
     }
 
@@ -51,25 +35,26 @@ namespace WebApi.Controllers
     [HttpGet("{id}")]
     public Book GetBooksById(int id)
     {
-      var book = BookList.Where(x => x.Id == id).SingleOrDefault();
+      var book = _context.Books.Where(x => x.Id == id).SingleOrDefault();
       return book;
     }
 
     [HttpPost]
     public IActionResult AddBook([FromBody] Book newBook)
     {
-      var book = BookList.SingleOrDefault(x => x.Title == newBook.Title);
+      var book = _context.Books.SingleOrDefault(x => x.Title == newBook.Title);
       if (book is not null)
         return BadRequest();
 
-      BookList.Add(newBook);
+      _context.Books.Add(newBook);
+      _context.SaveChanges();
       return Ok();
     }
 
     [HttpPut("{id}")]
     public IActionResult UpdateBook(int id, [FromBody] Book updateBook)
     {
-      var book = BookList.SingleOrDefault(x => x.Id == id);
+      var book = _context.Books.SingleOrDefault(x => x.Id == id);
       if (book is null)
         return BadRequest();
 
@@ -78,16 +63,19 @@ namespace WebApi.Controllers
       book.PublishDate = updateBook.PublishDate != default ? updateBook.PublishDate : book.PublishDate;
       book.Title = updateBook.Title != default ? updateBook.Title : book.Title;
 
+      _context.SaveChanges();
+
       return Ok();
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteBook(int id)
     {
-      var book = BookList.SingleOrDefault(x => x.Id == id);
+      var book = _context.Books.SingleOrDefault(x => x.Id == id);
       if (book is null)
         return BadRequest();
-      BookList.Remove(book);
+      _context.Books.Remove(book);
+      _context.SaveChanges();
       return Ok();
     }
   }
--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -15,6 +15,32 @@ namespace WebApi.DBOperations
         {
           return;
         }
+
+        context.Books.AddRange(
+          new Book
+          {
+            Title = "Lean Startup",
+            GenreId = 1, // Personal Growth,
+            PageCount = 200,
+            PublishDate = new DateTime(2001, 06, 12)
+          },
+          new Book
+          {
+            Title = "Herland",
+            GenreId = 2, // Science Finction
+            PageCount = 250,
+            PublishDate = new DateTime(2010, 05, 23)
+          },
+          new Book
+          {
+            Title = "Dune",
+            GenreId = 2, // Personal Growth,
+            PageCount = 200,
+            PublishDate = new DateTime(2001, 12, 21)
+          }
+        );
+
+        context.SaveChanges();
       }
     }
   }
